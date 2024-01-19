11cd3dfe81fd9e382b0af6a071ddb6e63191adcb
Ingo Montarroyos <ingo.montarroyos@truewindglobal.com>
2024-01-19 12:33:57 -0300

    Fixed the ZoomVideo.js file and added the reference to Zoom Video SDK through gradle.

--- www/ZoomVideo.js
+++ www/ZoomVideo.js
@@ -1,19 +1,17 @@
-cordova.define("cordova.plugin.zoomvideo.ZoomVideo", function(require, exports, module) {
-    var exec = require('cordova/exec');
-    var PLUGIN_NAME = "ZoomVideo";
+var exec = require('cordova/exec');
+var PLUGIN_NAME = "ZoomVideo";
 
-    function callNativeFunction(name, args, success, error) {
-        success = success || function(){};
-        error = error || function(){};
-        args = args || [];
-        exec(success, error, PLUGIN_NAME, name, args);
+function callNativeFunction(name, args, success, error) {
+    success = success || function(){};
+    error = error || function(){};
+    args = args || [];
+    exec(success, error, PLUGIN_NAME, name, args);
+}
+
+var zoom = {
+    openSession: function(jwtToken, sessionName, userName, domain, enableLog, success, error) {
+        callNativeFunction('openSession', [jwtToken, sessionName, userName, domain, enableLog], success, error);
     }
+};
 
-    var zoom = {
-        openSession: function(jwtToken, sessionName, userName, domain, enableLog, success, error) {
-            callNativeFunction('openSession', [jwtToken, sessionName, userName, domain, enableLog], success, error);
-        }
-    };
-
-    module.exports = zoom;
-});
+module.exports = zoom;
