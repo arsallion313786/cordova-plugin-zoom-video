ead4d481842a98ba08e5ebd50b3f2650dca691f3
arsallion313786 <99034072+arsallion313786@users.noreply.github.com>
2024-10-07 14:59:04 +0500

    share screen extension class added

---
+++ src/ios/SampleHandler.h
@@ -0,0 +1,12 @@
+//
+//  SampleHandler.h
+//  ScreenShare
+//
+//  Created by Muhammad Arslan Khalid on 03/10/2024.
+//
+
+#import <ReplayKit/ReplayKit.h>
+
+@interface SampleHandler : RPBroadcastSampleHandler
+
+@end
